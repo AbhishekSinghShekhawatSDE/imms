4d91f7594418f9b8495d0f2bdc5571ed43fa9282
AbhishekSinghShekhawatSDE <abhisheksinghshekhawat63@gmail.com>
2026-03-08 16:19:26 +0530

    chore: remove debug tools

--- backend/check-tables.js
+++
@@ -1,5 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query("SELECT tablename FROM pg_tables WHERE schemaname = 'public'")
-    .then(r => { console.log(r.rows); pool.end(); })
-    .catch(e => { console.error(e.message); pool.end(); });
